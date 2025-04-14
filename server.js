aff902fd0abf6d52682567e811381920fe32e12e
muralidharan7787 <muralidharan25aug@gmail.com>
2025-04-14 11:55:03 +0530

    Without db

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./routes/users');
+// const userRoutes = require('./routes/users');
 
 const app = express();
 const PORT = 3000;
@@ -10,7 +10,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/users', userRoutes);
+// app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
